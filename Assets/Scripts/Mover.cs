7d533bc6521e614e66b8f7ef655cb7fa4343ac4d
MisterShake2099 <mistershake2099@gmail.com>
2020-02-04 20:56:36 -0800

    Just some tinkering and time-wasting.

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public Vector3 direction = Vector3.up;
+    float speed = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var movement = direction * speed;
+
+        this.transform.Translate(movement);
+    }
+
+}
